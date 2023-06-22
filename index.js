2281ae94095a1749edfd9815be4714ddb93a51a2
BustyBee <yrfrnhgurk@proton.me>
2023-06-22 18:22:34 +0200

    Send stats to discord webhook

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const axios = require('axios');
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -14,6 +16,7 @@ app.get('/entry/:entryname', (req, res) => {
     const entry = req.params.entryname.replace(/ /g,"_").toLowerCase();
 
     requestNum += 1;
+    sendToWebhook(requestNum, req.ip, 'entry/'+entry);
     console.log(`${requestNum} requests since last build | IP ${req.ip} requested /entry/${entry}`)
 
     res.status(200).send(data[entry]);
@@ -22,7 +25,8 @@ app.get('/entry/:entryname', (req, res) => {
 app.get('/all/', (req, res) => {
 
     requestNum += 1;
-    console.log(`${requestNum} requests since last build | IP ${req.ip} /all`)
+    sendToWebhook(requestNum, req.ip, 'all');
+    console.log(`${requestNum} requests since last build | IP ${req.ip} /all`);
 
     res.status(200).send(data);
 });
@@ -32,3 +36,31 @@ app.listen(
     PORT,
     () => console.log(`API alive on port ${PORT}`)
 );
+
+function sendToWebhook(reqnum,ip, endoint){
+    axios.post(process.env.WEBHOOKURL, {
+        username: "TotK Compendium API",
+        avatar_url: "",
+        embeds: [
+            {
+                "title": `${reqnum} requests since last build`,
+                "color": 15258703,
+                "fields": [
+                    {
+                        "name": "Request by",
+                        "value": ip,
+                        "inline": true
+                    },
+                    {
+                        "name": "Endpoint",
+                        "value": endoint,
+                        "inline": true
+                    }
+                ]
+            }
+        ]
+    })
+      .catch(function (error) {
+        console.log(error.message);
+      });
+}
