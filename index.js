fbba669c3003a0dca7873899ac2ae9030ccd0eab
BustyBee <yrfrnhgurk@proton.me>
2023-06-22 17:41:58 +0200

    Added request-counter

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+let requestNum = 0;
+
 app.set('trust proxy', true)
 
 const data = require('./COMPENDIUM.json');
@@ -11,12 +13,16 @@ app.get('/entry/:entryname', (req, res) => {
 
     const entry = req.params.entryname.replace(/ /g,"_").toLowerCase();
 
-    console.log(`User with IP ${req.ip} requested /entry/${entry}`)
+    requestNum += 1;
+    console.log(`${requestNum} requests since last build | IP ${req.ip} requested /entry/${entry}`)
 
     res.status(200).send(data[entry]);
 });
 
 app.get('/all/', (req, res) => {
+
+    requestNum += 1;
+    console.log(`${requestNum} requests since last build | IP ${req.ip} /all`)
 
     res.status(200).send(data);
 });
