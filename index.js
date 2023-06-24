2596e45d6683a39f3c116c090da7da5a51961dfe
BustyBee <yrfrnhgurk@proton.me>
2023-06-24 23:27:03 +0200

    Added secret entry

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.get('/entry/:entryname', (req, res) => {
 
     const entry = req.params.entryname.replace(/ /g,"_").toLowerCase();
 
+    if (entry === process.env.SECRET_ENTRY) return res.status(200).send({name: process.env.SECRET_ENTRYTITLE, description: process.env.SECRET_ENTRYDESC, image: process.env.SECRET_ENTRYIMG, locations: [process.env.SECRET_ENTRYLOCATIONS_A, process.env.SECRET_ENTRYLOCATIONS_B]});
+
     sendToWebhook(req.ip, '/entry/'+entry);
     console.log(`New request | IP ${req.ip} requested /entry/${entry}`)
 
